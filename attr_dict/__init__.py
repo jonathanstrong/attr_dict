c8ca2e8aaae9d4aaec601b71843cfd39fcb3ef6e
Jonathan Strong <jonathan.strong@gmail.com>
2016-02-26 15:06:25 -0500

    added recursive walk constructor

--- attr_dict/__init__.py
+++ attr_dict/__init__.py
@@ -3,3 +3,11 @@ class AttrDict(dict):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
+    @classmethod
+    def recursive_walk(cls, d): 
+        self = cls(d)
+        for key, value in self.items():
+            if type(value) is dict: 
+                self[key] = cls.recursive_walk(value)
+        return self
+
